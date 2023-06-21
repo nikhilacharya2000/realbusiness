08679aea38744ee4bceb56a99752baf64f6588f9
rahulyuvmedia <rahulyuvmedia@gmail.com>
2023-06-21 18:33:38 +0530

    farzi

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,14 +1,67 @@
-<div class="app-footer">
-    <div class="col-12 widget-content bg-premium-dark text-center rounded-top"
-    style="bottom: 0; color: #fff; font-weight: bold;padding: 10px;">
-    Reserved all copyrights ©|| &nbsp; {{date('Y')}}
-</div>
-</div>
-<!-- SlimScroll -->
-<script src="{{ asset('assets/js/main.js') }}"></script>
+<!-- <footer class="container-fluid gx-0 text-center text-lg-start text-muted">
 
-<script>
-    setTimeout(function () {
-        $('.alert').fadeOut('slow');
-    }, 5000); // <-- time in milliseconds
+<style>
+  .red-link {
+    color: #3b78a1;
+  }
+
+  .blue-link {
+    color: #3b78a1;
+  }
+</style>
+    <button title="back-to-top-btn" type="button" class="btn btn-dark float-end m-3">
+        <a title="back to top" href="#">
+            <i class="bi bi-caret-up-fill back-to-top"></i>
+        </a>
+    </button>
+    <div class="d-flex " style="justify-content: center">
+
+
+        
+<a class="text-center p-4 red-link" href="/terms-conditions">Terms Conditions</a>
+<a class="text-center p-4 blue-link" href="/privacy-policy">Privacy Policy</a>
+
+
+        <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05) d-flex">
+            © 2023 Copyright: All Ai Apps.
+        </div>
+    </div>
+
+</footer>
+
+{{-- swiper js --}}
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+<!-- MDB -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.js"></script>
+
+{{-- jquery --}}
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+{{-- bootstrap - js --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+    integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous">
 </script>
+
+{{-- popperjs --}}
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+    integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous">
+</script>
+
+<!-- Share JS -->
+<script src="{{ asset('js/share.js') }}"></script>
+
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+{{-- disqus --}}
+<!-- <script id="dsq-count-scr" src="//allaiapps.disqus.com/count.js" async></script> -->
+
+{{-- <script src="{{ asset('/frontend-assets/js/bootstrap.min.js') }}"></script> --}}
+<script src="{{ asset('/frontend-assets/js/slider.js') }}"></script>
+<script src="{{ asset('/frontend-assets/js/main.js') }}"></script>
+
+<script type="text/javascript">
+    jssor_1_slider_init();
+</script> 
